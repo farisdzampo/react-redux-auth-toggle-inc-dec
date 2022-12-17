fd054d0e0550c61e22e11d142d7b67139846c861
Faris Dzampo <faredzampo23@gmail.com>
2022-12-17 19:01:39 +0100

    first-commit

---
+++ src/components/Counter.js
@@ -0,0 +1,39 @@
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggle());
+  };
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(5)); // vraca obj sa type:nesto i payload:5 tako da se u index.js umjesto amount sto sam radio bez toolkita trebalo staviti payload
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
